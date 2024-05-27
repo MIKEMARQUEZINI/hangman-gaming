b8cee303fdd1abcb88ffd5b67a5cfbbdb4811796
Mike Marquezini <mike.santosmarquezini@ab-inbev.com>
2024-05-27 16:04:43 -0300

    feat: Refactor code by introducing enum to improve readability and maintainability

---
+++ src/enum/EGaming.ts
@@ -0,0 +1,25 @@
+export enum GameStage {
+  Starting = "Starting",
+  Gaming = "Gaming",
+  Ending = "Ending",
+}
+
+export enum GameScreen {
+  Starter = "Home",
+  Game = "Game Mode",
+  Finish = "Game Over",
+}
+
+export enum Infos {
+  Category = "Category",
+  Points = "Points",
+  Attempts = "Attempts",
+  GuessTheSecretWord = "Guess the secret word in 5 tries or less!",
+  WrongLetters = "Wrong Letters:",
+  Try = "Try again!",
+  YouWin = "You win!",
+  YouLose = "You lose!",
+  Go = "Let's Go!",
+  Next = "Next",
+  ClickHere = "Click here to start",
+}
