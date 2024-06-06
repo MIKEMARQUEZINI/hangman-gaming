11d4f94f21267b15fa6c747f497998a64ebd3a3b
Mike Marquezini <mike.santosmarquezini@ab-inbev.com>
2024-06-06 20:17:42 -0300

    refactor: adjusting styles

--- src/components/Game/styles.ts
+++ src/components/Game/styles.ts
@@ -1,13 +1,30 @@
 import styled from "styled-components";
 import { Button, Container, centralize } from "../../styles/Globals";
 
-export const WordContainer = styled.div`
-  margin: 1.5em;
+const BoldText = styled.span`
+  font-weight: bold;
+`;
+
+const Square = styled.span`
+  font-size: 50px;
+  line-height: 1.5;
+  text-align: center;
+  width: 100%;
+  height: 100%;
+  color: #0e3806;
+  background: #fff;
+  font-weight: bold;
+`;
+
+const ContainerWithMarginAndPadding = styled(Container)`
   padding: 1.5em;
+  margin: 1.5em;
+`;
+
+export const WordContainer = styled(ContainerWithMarginAndPadding)`
   border: 10px solid #0e3806;
   display: flex;
   border-radius: 8px 8px 8px 8px;
-  flex-direction: row;
 `;
 
 export const LetterContainer = styled.form`
@@ -22,40 +39,32 @@ export const Input = styled.input`
   margin-right: 1em;
 `;
 
-export const Title = styled.span``;
+export const Title = BoldText;
 
 export const Text = styled.p``;
 
-export const infos = styled.span`
+export const infos = styled(BoldText)`
   ${centralize}
   color: #333;
   font-size: 1.2rem;
   padding: 10px;
-  font-weight: bold;
 `;
 
-export const Points = styled.span`
-  font-weight: bold;
-`;
+export const Points = BoldText;
 
-export const Content = styled(Container)`
-  padding: 1.5em;
-  margin: 1.5em;
-`;
+export const Content = ContainerWithMarginAndPadding;
 
 export const ButtonGaming = styled(Button)`
   margin-top: 1em;
 `;
 
-export const letter = styled.span`
-  font-size: 70px;
-  line-height: 1.5;
+export const letter = styled(Square)`
   border: 13px solid #0e3806;
-  text-align: center;
-  width: 100%;
-  height: 100%;
   text-transform: uppercase;
-  background: #fff;
-  color: #0e3806;
-  font-weight: bold;
+`;
+
+export const EmptySquare = styled(Square)`
+  border: 2px solid #0e3806;
+  width: 1%;
+  height: 1%;
 `;
